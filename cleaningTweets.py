2ce90b2c90037b83c296f5afc0a97ba1001ab398
lucasvejar <lucasficus@gmail.com>
2020-08-19 22:13:13 -0300

    agregue limpieza de elongatd_tweets

--- cleaningTweets.py
+++ cleaningTweets.py
@@ -13,6 +13,7 @@ class Cleaner():
         tokens = parser(sentence)
         tokens = [ word.lemma_.lower().strip() if word.lemma_ != '-PRON-' else word.lower_ for word in tokens ]
         tokens = [ word for word in tokens if word not in STOP_WORDS and word not in punctuation ]
+        tokens = [ self.delete_elongated_words(word) for word in tokens ]   # converting elongated words into normal words
         # tokens = list(tokens)  # removing the duplicates tokens ? 
         return tokens
 
@@ -39,12 +40,46 @@ class Cleaner():
         tweet = re.sub(r'[0-9]+','',tweet)
         return tweet
 
+
+    def replace_elongated_word(self,word):
+        i=0
+        count = 0
+        found=False
+        while not found and i<len(word)-1:
+            if word[i] == word[i+1]:
+                if count == 0:
+                    inicio = i
+                count = count +1
+                end = i+1
+            else:
+                if count > 2:
+                    found = True
+                    end = i
+                else:
+                    if count>0:
+                        count = 0
+            i = i+1
+            if i==len(word)-1 and count >2 and word.endswith(word[i-1]):
+                end = len(word)
+                break
+        return word[0:inicio+1] + word[end+1:]
+
+    def delete_elongated_words(self,word):
+        reg = re.compile(r'(.)\1{2}')  # r'(\w*)(\w+)(\2)(\w*)'
+        return self.replace_elongated_word(word) if reg.search(word) else word
+        #elongated = [ self.replace_elongated_word(word) for word in tweet.split() if reg.search(word) ]
+        #non_elongated_tweet = [ self.replace_elongated_word(word) for word in elongated ]
+        #return non_elongated_tweet
+
     def prepare_data(self, tweets):
         return [ self.spacy_tokenizer(tweet) for tweet in tweets]
 
 
 # This was a test code 
-# if __name__ == '__main__':    
-#    tweet = "@GolosoAlonso New #dotnotes! Today we're reviewing database fundamentals, including OLTP vs OLAP + data organization and 1 more. https://meet.google.com/rue. This is the new beggining"
-#    print('original tweet: ',tweet)
- #   print('clean tweet: ',prepare_data(tweet))
+if __name__ == '__main__':    
+    cl = Cleaner()
+    tweet = "@GolosoAlonso New #dotnotes! Todaaaay we're reviewing database fundamentals, including OLTP vs OLAP + data organization and 1 more. https://meet.google.com/rue. This is the new beggining"
+    print('original tweet: ',tweet)
+    print('clean tweet: ',cl.prepare_data([tweet]))
+
+    #print(cl.replace_elongated_word('aaaaaal'))
